b87f466f58f6484532d22d5b6106a5c40e9ad6da
dluke-oc <117632474+dluke-oc@users.noreply.github.com>
2022-12-03 12:26:21 -0500

    Update and rename helpisnice4.py to helpisnice5.py

--- helpisnice5.py
+++ helpisnice5.py
@@ -28,7 +28,7 @@ def vaisseau_deplacement(x, y):
     return x, y
 
 def ball_movement(x, y):
-    global xball_speed, yball_speed
+    global xball_speed, yball_speed, vaisseau_x
     x -= xball_speed
     y -= yball_speed
     if (x < 5) or (x > 246):
