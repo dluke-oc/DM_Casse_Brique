4ef5d29e2a776252363c277261aafff7ff901d5a
dluke-oc <117632474+dluke-oc@users.noreply.github.com>
2022-12-03 12:10:28 -0500

    Update and rename koolaidisbackbro2.py to koolaidisbackbro3.py

--- koolaidisbackbro3.py
+++ koolaidisbackbro3.py
@@ -20,7 +20,7 @@ def vaisseau_deplacement(x, y):
     """déplacement avec les touches de directions"""
 
     if pyxel.btn(pyxel.KEY_RIGHT):
-        if (x < 240) :
+        if (x < 230) :
             x = x + 2
     if pyxel.btn(pyxel.KEY_LEFT):
         if (x > 20) :
