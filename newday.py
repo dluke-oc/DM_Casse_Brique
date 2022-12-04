2036c63edda25378aaa6af487355c630f63f9a53
dluke-oc <117632474+dluke-oc@users.noreply.github.com>
2022-12-04 04:54:37 -0500

    Update and rename progress25.py to newday.py

--- newday.py
+++ newday.py
@@ -47,6 +47,10 @@ def ball_movement(x, y):
             ball_y = ball_y + 5
             xball_speed = xball_speed *1.015
             yball_speed = -yball_speed*1.015
+    if (31 * 2) < y < ((52*2) + (7*2)):
+        if (19*2) < x < ((94*2)+(15*2)):            
+            xball_speed = xball_speed *1.015
+            yball_speed = -yball_speed*1.015       
     else:
         xball_speed = xball_speed
         yball_speed = yball_speed
