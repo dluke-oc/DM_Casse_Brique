e6e4e830eb5300cffa31da6bbf44781076a59057
dluke-oc <117632474+dluke-oc@users.noreply.github.com>
2022-12-04 05:13:37 -0500

    Update and rename newday8.py to newday9.py

--- newday9.py
+++ newday9.py
@@ -41,12 +41,12 @@ def ball_movement(x, y):
     if  215 <= y <= (238):
         if (vaisseau_x -20) <= x < (vaisseau_x):
             ball_y = ball_y + 5
-            xball_speed = -xball_speed*1.015
-            yball_speed = -yball_speed*1.015
+            xball_speed = -xball_speed#*1.015
+            yball_speed = -yball_speed#*1.015
         elif vaisseau_x <= x <= (vaisseau_x +55):
             ball_y = ball_y + 5
-            xball_speed = xball_speed *1.015
-            yball_speed = -yball_speed*1.015
+            xball_speed = xball_speed #*1.015
+            yball_speed = -yball_speed#*1.015
     if (31) <= y <= (52*2) and (19*2) <= x <= ((94*2)+(15*2)):
         xball_speed = xball_speed 
         yball_speed = -yball_speed       
