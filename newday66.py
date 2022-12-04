0da2cfc2ea31ac36bb30646e2712d5d6a00b2644
dluke-oc <117632474+dluke-oc@users.noreply.github.com>
2022-12-04 09:38:15 -0500

    Update and rename newday65.py to newday66.py

--- newday66.py
+++ newday66.py
@@ -66,7 +66,11 @@ def ball_movement(x, y):
     #if ball_y == exbtom and exleft <= ball_x <= exright: #rebond contre brique au-dessus
      #   xball_speed = xball_speed
       #  yball_speed = -yball_speed
-    
+    if brick_y[0] <= ball_y <= (brick_y[11]+14):
+        for l in range (12):
+            if brick_x[l -1] <= x <= (brick_x[l-1]+30):
+                xball_speed = xball_speed 
+                yball_speed = -yball_speed    
     else:
         xball_speed = xball_speed
         yball_speed = yball_speed
