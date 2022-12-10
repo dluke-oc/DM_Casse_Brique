617a683507110d68819585e5b26dca43f978f37a
dluke-oc <117632474+dluke-oc@users.noreply.github.com>
2022-12-10 11:56:41 -0500

    Update and rename juice29.py to juice30.py

--- juice30.py
+++ juice30.py
@@ -61,6 +61,15 @@ def ball_movement(x, y):
             ball_y = ball_y + 5
             xball_speed = xball_speed
             yball_speed = -yball_speed
+    if  215 <= y <= (238) and len(totbrick) <= 3:
+        if (vaisseau_x -20) <= x < (vaisseau_x-2) or (vaisseau_x + 35) < x <= (vaisseau_x + 55):
+            ball_y = ball_y + 5
+            xball_speed = -xball_speed*1.015
+            yball_speed = -yball_speed*1.015
+        elif vaisseau_x <= x <= (vaisseau_x +32):
+            ball_y = ball_y + 5
+            xball_speed = xball_speed*1.015
+            yball_speed = -yball_speed*1.015
     
     return x, y
 
