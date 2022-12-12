7cc47a12231dfc2e9012ae13d84879c052696420
dluke-oc <117632474+dluke-oc@users.noreply.github.com>
2022-12-12 00:50:30 -0500

    Update and rename juice47.py to juice48.py

--- juice48.py
+++ juice48.py
@@ -118,7 +118,7 @@ def update():
     game, vie = life(game, vie)
     win = victory(win)
     if game == False:
-        ball_x, ball_y = (vaisseau_x + 18), (vaisseau_y - 4)
+        ball_x, ball_y = (vaisseau_x + 18), (vaisseau_y - 7)
     if pyxel.btnr(pyxel.KEY_SPACE):
         game = True
         rule = False
@@ -151,8 +151,8 @@ def draw():
         pyxel.rectb(brick_corx[i], brick_cory[i], 30, 14, brick_col[i])
         
     if rule == True:
-        pyxel.text(32, 150, "Les briques jaunes font accelerer la balle et contamine les autres briques!", 10)
-        pyxel.text(32, 180, "Essayer de les garder pour la fin", 10)
+        pyxel.text(10, 150, "Les briques jaunes font accelerer la balle et contamine les autres briques!", 10)
+        pyxel.text(60, 180, "Essayer de les garder pour la fin", 10)
     if vie == 0:
         pyxel.cls(0)
         pyxel.text(110, 128, "Game Over", 7)
